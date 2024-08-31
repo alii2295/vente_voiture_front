ec7ac73d41fc824602680855d7b4ddbb35578b5a
ryhab <ryhabelmoncerr@gmail.com>
2024-08-30 18:30:44 -0600

    Description des changements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent implements OnInit {
       this.logged = isLoggedIn;
     });
   }
-
+//////////////////////
   logout() {
     this.authService.logout();
     this.router.navigate(['/login']);
